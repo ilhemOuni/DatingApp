2dcd232a1a7e360594a21234b1ba5f19cc66e28a
ILHEM EL OUNI <elouniilhem@gmail.com>
2021-04-16 05:00:55 +0100

    End of section 13

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { Pagination } from 'src/app/_models/pagination';
+import { User } from 'src/app/_models/user';
+import { UserParams } from 'src/app/_models/userParms';
 import { Member } from 'src/app/_modules/member';
+import { AccountService } from 'src/app/_services/account.service';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
@@ -10,13 +15,37 @@ import { MembersService } from 'src/app/_services/members.service';
 })
 export class MemberListComponent implements OnInit {
 
-  members$: Observable<Member[]>;
+  members: Member[];
+  pagination: Pagination;
+  userParams : UserParams;
+  user: User;
+  genderList =[{value:'male', display:'Male'}, {value:'female', display:'Female'}]
   
-  constructor(private memberService : MembersService) { }
+  constructor(private memberService : MembersService) {
+    this.userParams = this.memberService.getUserParams();
+   }
 
   ngOnInit(): void {
-    this.members$ = this.memberService.getMembers();  
+    this.loadMembers();
   }
   
+  loadMembers(){
+    this.memberService.setUserParams(this.userParams);
+    this.memberService.getMembers(this.userParams).subscribe(response =>{
+      this.members = response.result,
+      this.pagination = response.pagination
+    })
+  }
+
+  reseltFilters() {
+    this.userParams = this.memberService.resetUserParams();
+    this.loadMembers();
+  }
+
+  pageChanged(event:any){
+    this.userParams.pageNumber = event.page;
+    this.memberService.setUserParams(this.userParams);
+    this.loadMembers();
+  }
   
 }
